eaa98bc85799a77cf7c880f034791ff7000d2573
Zagitalc <zachary.13281@gmail.com>
2021-12-04 21:47:27 +0000

    Update chartsnfigures.js

--- chartsnfigures.js
+++ chartsnfigures.js
@@ -1,47 +1,49 @@
 //chart.js charts used in dataset
 
-const chartData = 'hurricane data from noaa selected.csv';
-d3.csv(chartData).then(function(ds){
-    console.log(ds)
-});
-    
-
-    
-
 const ctx = document.getElementById('myChart1');
-    const myChart1 = new Chart(ctx, {
-        type: 'bar', 
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+    const chartData = 'hurricane data from noaa selected.csv';
+    d3.csv(chartData).then(function(ds){
+        console.log(ds)
+        const date = [];
+        const windspd = [];
+        const name = [];
+
+        for (i=0; i < ds.length; i++) {
+            date.push(ds[i].date)
+            windspd.push(ds[i].windspd)
+            name.push(ds[i].name)
+        }
+
+        
+
+        
+
+        const myChart1 = new Chart(ctx, {
+            type: 'bar', 
+            data: {
+                //labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: windspd,
+                    backgroundColor: 
+                        'rgba(255, 99, 132, 0.2)',
+                        
+                    
+                    borderColor: 
+                        
+                        'rgba(153, 102, 255, 1)',
+                        
+                    
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
                 }
             }
-        }
+        });
     });
-
-   
+    
