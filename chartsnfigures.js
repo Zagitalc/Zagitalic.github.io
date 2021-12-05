572d8ee7054ef8e2ed65ae1a28f2d4e1a38d8f02
Zagitalc <zachary.13281@gmail.com>
2021-12-05 12:16:54 +0000

    Update chartsnfigures.js

--- chartsnfigures.js
+++ chartsnfigures.js
@@ -1,53 +1,5 @@
-//chart.js charts used in dataset
-/*
-const ctx = document.getElementById('myChart1');
-    const chartData = 'hurricane data from noaa selected.csv';
-    d3.csv(chartData).then(function(ds){
-        console.log(ds)
-        const date = [];
-        const windspd = [];
-        const name = [];
+//d3.js charts used in dataset
 
-        for (i=0; i < ds.length; i++) {
-            date.push(ds[i].date)
-            windspd.push(ds[i].windspd)
-            name.push(ds[i].name)
-        }
-
-        
-
-        
-
-        const myChart1 = new Chart(ctx, {
-            type: 'bar', 
-            data: {
-                //labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: windspd,
-                    backgroundColor: 
-                        'rgba(255, 99, 132, 0.2)',
-                        
-                    
-                    borderColor: 
-                        
-                        'rgba(153, 102, 255, 1)',
-                        
-                    
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    });
- */
-    
 
 
 // set the dimensions and margins of the graph
