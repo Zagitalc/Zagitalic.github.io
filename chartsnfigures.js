7bddbf0e42e6f5683c0ec7af5e870f6ed62dee7c
Zagitalc <zachary.13281@gmail.com>
2021-12-05 16:41:59 +0000

    1

--- chartsnfigures.js
+++ chartsnfigures.js
@@ -10,17 +10,9 @@ var margin = {top: 30, right: 30, bottom: 70, left: 60},
 
 
 
- // set the ranges
-var x = d3.scaleBand()
-.range([0, width])
-.padding(0.1);
-var y = d3.scaleLinear()
-.range([height, 0]);
+ 
 
-// append the svg object to the body of the page
-
-
-// append the svg object to the body of the page
+// append svg to body
 var svg = d3.select("#windspeed_chart").append("svg")
 .attr("width", width + margin.left + margin.right)
 .attr("height", height + margin.top + margin.bottom)
@@ -28,45 +20,54 @@ var svg = d3.select("#windspeed_chart").append("svg")
 .attr("transform", 
 "translate(" + margin.left + "," + margin.top + ")");
 
-// get the data
-d3.csv("hurricane data from noaa selected.csv", function(error, data) {
-if (error) throw error;
-
-// format the data
-data.forEach(function(d) {
-d.Windspeed = +d.Windspeed;
-});
-
-// Scale the range of the data in the domains
-x.domain(data.map(function(d) { return d.Date; }));
-y.domain([50, d3.max(data, function(d) { return d.Windspeed; })]);
+//set variables x, y, X and Y axes 
+var x = d3.scaleBand()
+.range([0, width])
+.padding(0.1);
+var xAxis = svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
 
 
-// append the rectangles for the bar chart
-svg.selectAll(".bar")
-.data(data)
-.enter()
-.append("rect")
-.attr("class", "bar")
-.attr("x", function(d) { return x(d.Date); })
-.attr("width", x.bandwidth())
-.attr("y", function(d) { return y(d.Windspeed); })
-.attr("height", function(d) { return height - y(d.Windspeed); });
 
-  
+var y = d3.scaleLinear()
+.range([height, 0]);
+var yAxis = svg.append("g")
+  .attr("class", "myYaxis")
 
-// add the x Axis
-svg.append("g")
-.attr("transform", "translate(0," + height + ")")
-.call(d3.axisBottom(x))
-.selectAll("text")
-.attr("transform", "translate(-10,0)rotate(-45)")
-.style("text-anchor", "end");
+// update the data once onclick event occurs
+function update(selectedVar) {
+    d3.csv("hurricane data change.csv", function(data) {
 
-// add the y Axis
-svg.append("g")
 
-.call(d3.axisLeft(y));
 
-});
+    // append the rectangles for the bar chart
+    // X axis
+    x.domain(data.map(function(d) { return d.Decade; }))
+    xAxis.transition().duration(1000).call(d3.axisBottom(x))
 
+    // Add Y axis
+    y.domain([0, d3.max(data, function(d) { return +d[selectedVar] }) ]);
+    yAxis.transition().duration(1000).call(d3.axisLeft(y));
+
+    // variable u: map data to existing bars
+    var u = svg.selectAll("rect")
+    .data(data)
+
+    // update bars
+    u
+    .enter()
+    .append("rect")
+    .merge(u)
+    .transition()
+    .duration(1000)
+        .attr("x", function(d) { return x(d.Decade); })
+        .attr("y", function(d) { return y(d[selectedVar]); })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height - y(d[selectedVar]); })
+        .attr("fill", "#69b3a2")
+    })
+
+}
+
+// Initialize plot
+update('Frequency')
