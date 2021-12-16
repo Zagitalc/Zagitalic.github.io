bdcbb61c7ab2ec3c46fd4168b677f91794b99c91
Zagitalc <zachary.13281@gmail.com>
2021-12-16 13:02:53 +0000

    Update chartsnfigures.js

--- chartsnfigures.js
+++ chartsnfigures.js
@@ -2,18 +2,90 @@
 
 
 
+
+
 // set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+
+  // When reading the csv, I must format variables:
+  function(d){
+    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+  },
+
+  // Now I can use this dataset:
+  function(data) {
+
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// set the dimensions and margins of the graph
+/*
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
     width = 1020 - margin.left - margin.right,
     height = 800 - margin.top - margin.bottom;
-
+*/
 
 
 
  
 
 // append svg to body
-var svg = d3.select("#windspeed_chart").append("svg")
+var svg2 = d3.select("#windspeed_chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
@@ -24,7 +96,7 @@ var svg = d3.select("#windspeed_chart").append("svg")
 var x = d3.scaleBand()
     .range([0, width])
     .padding(0.1);
-var xAxis = svg.append("g")
+var xAxis = svg2.append("g")
     .attr("transform", "translate(0," + height + ")")
 
 const xAxisG = xAxis.append("text")
@@ -38,7 +110,7 @@ const xAxisG = xAxis.append("text")
 
 var y = d3.scaleLinear()
     .range([height, 0]);
-var yAxis = svg.append("g")
+var yAxis = svg2.append("g")
     .attr("class", "myYaxis")
 
 // update the data once onclick event occurs
@@ -57,7 +129,7 @@ function update(selectedVar) {
     yAxis.transition().duration(1000).call(d3.axisLeft(y));
 
     // variable u: map data to existing bars
-    var u = svg.selectAll("rect")
+    var u = svg2.selectAll("rect")
     .data(data)
 
     // update bars
@@ -76,5 +148,16 @@ function update(selectedVar) {
 
 }
 
+
+
+
+
+
+
+
+
+
+
+//main
 // Initialize plot
 update('Frequency')
