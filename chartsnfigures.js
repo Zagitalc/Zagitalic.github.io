1a9f126a44d8cfa5fdeeea775bb4fe631bd6cfbb
Zagitalc <zachary.13281@gmail.com>
2021-12-18 11:03:02 +0000

    1

--- chartsnfigures.js
+++ chartsnfigures.js
@@ -24,9 +24,9 @@ function mouseOver(btnName,newVar){
 function mouseOut(btnName,revertText){
     document.getElementById(btnName).innerText=revertText
 }
-hover("button1",'Frequency','Frequency of Hurricanes')
-hover("button2",'FreqOverhundred','Frequency of Hurricanes with windspeeds over 100mph')
-hover("button3",'percentage','Percentage (over 100mph / total no. of Hurricanes)' )
+hover("btnFreq",'Frequency','Frequency of Hurricanes')
+hover("btnFreq100",'FreqOverhundred','Frequency of Hurricanes with windspeeds over 100mph')
+hover("btnPercent",'percentage','Percentage (over 100mph / total no. of Hurricanes)' )
 hover("btnEnergy",'EnergyIndex','Accumulated Cyclone Energy (10^(-4) kt^2)' )
 hover("btnSeaTmp",'MeanSeaTemp','Mean Sea Temperature (Mean based on year 1990')
 /**
